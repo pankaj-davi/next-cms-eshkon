4b6a747c99e27237a0e6ec70bf260f6306a6935a
pankaj-davi <menariapankaj17@gmail.com>
2024-06-06 23:23:23 +0530

    init setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    STATSIG_CLIENT_KEY: process.env.STATSIG_CLIENT_KEY,
+  },
+};
 
 export default nextConfig;
